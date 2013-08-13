82af0b3c9f07575951c2b3a0a0a87db956dedeb7
ok100 <ok100@lavabit.com>
2013-08-13 14:07:56 +0200

    R.I.P lavabit

--- lyvi/utils.py
+++ lyvi/utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2013 Ondrej Kipila <ok100 at lavabit dot com>
+# Copyright (c) 2013 Ondrej Kipila <ok100 at openmailbox dot org>
 # This work is free. You can redistribute it and/or modify it under the
 # terms of the Do What The Fuck You Want To Public License, Version 2,
 # as published by Sam Hocevar. See the COPYING file for more details.
