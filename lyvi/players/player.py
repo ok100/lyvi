22db2ff115f2b1e6b4a6abc0511a31125e11d4b7
ok100 <ok100@lavabit.com>
2013-08-13 07:54:21 +0200

    Removed unused import

--- lyvi/players/player.py
+++ lyvi/players/player.py
@@ -3,9 +3,6 @@
 # terms of the Do What The Fuck You Want To Public License, Version 2,
 # as published by Sam Hocevar. See the COPYING file for more details.
 
-import lyvi
-
-
 class _Player:
     _running = True
     _state = 'stop'
