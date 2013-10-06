853e7ce75a93c56a046f05de4a24a14deb371407
ok100 <ok100@openmailbox.org>
2013-10-06 14:56:13 +0200

    Refactor

--- lyvi/background.py
+++ lyvi/background.py
@@ -18,6 +18,32 @@ for line in check_output('xrdb -query').splitlines():
         BG_COLOR = line.split(':')[1].strip()
 
 
+def pil_image(image):
+    if isinstance(image, bytes):
+        buf = BytesIO(image)
+        return Image.open(buf)
+    return image
+
+
+def blend(image, opacity):
+    image = pil_image(image)
+    layer = Image.new(image.mode, image.size, BG_COLOR)
+    return Image.blend(image, layer, 1 - opacity)
+
+
+def paste(root, image_to_paste, x, y):
+    root = pil_image(root)
+    image_to_paste = pil_image(image_to_paste)
+    root.paste(image_to_paste, (x, y))
+    return root
+
+
+def resize(image, x, y):
+    image = pil_image(image)
+    image.thumbnail((x, y), Image.ANTIALIAS)
+    return image
+
+
 class Background:
     ESCAPE_STR_BEG = 'printf "\e]20;'
     ESCAPE_STR_END = ';100x100+50+50:op=keep-aspect\a"'
@@ -31,33 +57,34 @@ class Background:
         self.type = 'cover' if self.type == 'backdrops' else 'backdrops'
         self.update()
 
-    def blend(self, image, opacity):
-        if isinstance(image, bytes):
-            buf = BytesIO(image)
-            image = Image.open(buf)
-        layer = Image.new(image.mode, image.size, BG_COLOR)
-        return Image.blend(image, layer, 1 - opacity)
-
-    def update(self, clean=False):
+    def _make(self, clean=False):
         if (((self.type == 'backdrops' and lyvi.md.backdrops and lyvi.md.artist)
                 or (self.type == 'cover' and lyvi.md.cover and lyvi.md.album))
                 and not clean):
-            image = self.blend(getattr(lyvi.md, self.type), self.opacity)
+            image = blend(getattr(lyvi.md, self.type), self.opacity)
         else:
             image = Image.new('RGB', (100, 100), BG_COLOR)
         image.save(self.file)
+
+    def _set(self):
         os.system(self.ESCAPE_STR_BEG + self.file + self.ESCAPE_STR_END)
+
+    def update(self, clean=False):
+        self._make(clean=clean)
+        self._set()
 
     def cleanup(self):
         self.update(clean=True)
         os.remove(self.file)
 
 
-class TmuxBackground(Background):
-    ESCAPE_STR_BEG = 'printf "\ePtmux;\e\e]20;'   
-    ESCAPE_STR_END = ';100x100+50+50:op=keep-aspect\a\e\\\\"'
+class Tmux:
+    def __init__(self):
+        class Cell: pass
+        self.cell = Cell()
+        self.update()
 
-    def get_layout(self):
+    def _get_layout(self):
         class Pane: pass
         display = check_output('tmux display -p \'#{window_layout}\'')
         for delim in '[]{}':
@@ -77,51 +104,57 @@ class TmuxBackground(Background):
             layout[lsp.index(chunk) + 1].active = 'active' in chunk
         return layout
 
-    def get_size(self):
+    def _get_size(self):
         for line in check_output('xwininfo -name ' + lyvi.config['bg_tmux_window_title']).splitlines():
             if 'Width:' in line:
                 width = int(line.split()[1])
             elif 'Height:' in line:
                 height = int(line.split()[1])
         return width, height
+    
+    def update(self):
+        self.layout = self._get_layout()
+        self.width, self.height = self._get_size()
+        self.cell.w = round(self.width / self.layout[0].w)
+        self.cell.h = round(self.height / self.layout[0].h)
 
-    def paste(self, root, image, pane, cell_w, cell_h, blend):
-        buf = BytesIO(image)
-        image = Image.open(buf)
-        image.thumbnail((pane.w * cell_w, pane.h * cell_h), Image.ANTIALIAS)
-        if blend:
-            image = self.blend(image, self.opacity)
-        x1 = (pane.x - 1) * cell_w
-        y1 = (pane.y - 1) * cell_h
-        x2 = (pane.x + pane.w) * cell_w
-        y2 = (pane.y + pane.h) * cell_h
-        root.paste(image, (round(x1 + (x2 - x1) / 2 - image.size[0] / 2),
-                           round(y1 + (y2 - y1) / 2 - image.size[1] / 2)))
-        return root
 
-    def update(self, clean=False):
-        layout = self.get_layout()
-        width, height = self.get_size()
-        cell_w = width / layout[0].w
-        cell_h = height / layout[0].h
-        image = Image.new('RGB', (width, height), BG_COLOR)
+class TmuxBackground(Background):
+    ESCAPE_STR_BEG = 'printf "\ePtmux;\e\e]20;'   
+    ESCAPE_STR_END = ';100x100+50+50:op=keep-aspect\a\e\\\\"'
+
+    def __init__(self):
+        super().__init__()
+        self.tmux = Tmux()
+
+    def _make(self, clean=False):
+        self.tmux.update()
+        image = Image.new('RGB', (self.tmux.width, self.tmux.height), BG_COLOR)
         if not clean:
-            cover_data = [
-                lyvi.md.cover, 
-                layout[lyvi.config['bg_tmux_cover_pane'] + 1],
-                lyvi.config['bg_tmux_cover_underlying']
-            ]
-            backdrops_data = [
-                lyvi.md.backdrops, 
-                layout[lyvi.config['bg_tmux_backdrops_pane'] + 1],
-                lyvi.config['bg_tmux_backdrops_underlying']
-            ]
+            cover = {
+                'image': lyvi.md.cover, 
+                'pane': self.tmux.layout[lyvi.config['bg_tmux_cover_pane'] + 1],
+                'underlying': lyvi.config['bg_tmux_cover_underlying']
+            }
+            backdrops = {
+                'image': lyvi.md.backdrops, 
+                'pane': self.tmux.layout[lyvi.config['bg_tmux_backdrops_pane'] + 1],
+                'underlying': lyvi.config['bg_tmux_backdrops_underlying']
+            }
             if lyvi.config['bg_tmux_backdrops_pane'] == lyvi.config['bg_tmux_cover_pane']:
-                to_paste = [cover_data if self.type == 'cover' else backdrops_data]
+                to_paste = [cover if self.type == 'cover' else backdrops]
             else:
-                to_paste = [cover_data, backdrops_data]
-            for i in to_paste:
-                if i[0]:
-                    image = self.paste(image, i[0], i[1], cell_w, cell_h, i[2])
+                to_paste = [cover, backdrops]
+            for t in (t for t in to_paste if t['image']):
+                t['image'] = resize(t['image'], t['pane'].w * self.tmux.cell.w,
+                                                t['pane'].h * self.tmux.cell.h)
+                if t['underlying']:
+                    t['image'] = blend(t['image'], self.opacity)
+                x1 = t['pane'].x * self.tmux.cell.w
+                y1 = t['pane'].y * self.tmux.cell.h
+                x2 = (t['pane'].x + t['pane'].w) * self.tmux.cell.w
+                y2 = (t['pane'].y + t['pane'].h) * self.tmux.cell.h
+                x = round(x1 + (x2 - x1) / 2 - t['image'].size[0] / 2)
+                y = round(y1 + (y2 - y1) / 2 - t['image'].size[1] / 2)
+                image = paste(image, t['image'], x, y)
         image.save(self.file)
-        os.system(self.ESCAPE_STR_BEG + self.file + self.ESCAPE_STR_END)
